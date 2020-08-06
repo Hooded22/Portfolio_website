932c0ab638299e368c57b45f19243404ab3e0174
Przemek Sipta <przemek.sipta@gmail.com>
2020-08-06 13:00:13 +0200

    Update contact.js

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -73,7 +73,7 @@ const ContactSection = () => {
             return false;
         }
         else {
-            emailjs.sendForm('gmail', 'portfolio_website', event.target, process.env.GATSBY_EMAIL_USER_KEY)
+            emailjs.sendForm('gmail', 'portfolio_website', event.target, 'user_bcEBfS0Bds18cXA5tkfVk')
                 .then((result) => {
                     showNotificationBox(true, successNotifications.emailSended);
                 }, (error) => {
