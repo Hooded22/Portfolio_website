3d37e4cc7f7d8915f635da78bc42b945cae9c811
Przemek Sipta <przemek.sipta@gmail.com>
2020-08-06 14:39:03 +0200

    Scrollbar color and reapir list

--- src/components/navigations.js
+++ src/components/navigations.js
@@ -5,63 +5,67 @@ import { RiMenu5Line, RiCloseCircleLine } from "react-icons/ri"
 import { Link } from "gatsby"
 
 const DesktopMenu = ({ language, goToSection, openMenu }) => {
-    return (
-      <>
-        <nav className="navbarTop">
+  return (
+    <>
+      <nav className="navbarTop">
+        <ul>
+          {
+            PageSections.map(item => (
+              <li key={item.name}>
+                <Link className="link" to={`#${item.link}`} onClick={(event) => goToSection(event, item.link)}>
+                  {language == "PL" ? item.name : item.link}
+                </Link>
+              </li>
+            ))
+          }
+          <li>
+            <LanguageSelector />
+          </li>
+        </ul>
+      </nav>
+      <div className="mobileIconWrapper">
+        <RiMenu5Line
+          size={24}
+          className="mobileMenuIcon"
+          title="Open menu"
+          onClick={() => openMenu()}
+        />
+      </div>
+    </>
+  );
+}
+
+const MobileMenu = ({ mobileMenuDisplay, mobileMenuContentClass, closeMenu, goToSection, language }) => {
+  return (
+    <div className="mobileMenuOverlay" style={{ display: mobileMenuDisplay }}>
+      <div className={`mobileMenuWrapper ${mobileMenuContentClass}`}>
+        <div className="header">
+          <RiCloseCircleLine
+            size={34}
+            color="#FAFAFA"
+            title="Close menu"
+            onClick={() => closeMenu()}
+          />
+        </div>
+        <div className="content">
           <ul>
-            {
-              PageSections.map(item => (
+            {PageSections.map(item => {
+              return (
                 <li key={item.name}>
-                  <Link className="link" to={`#${item.link}`} onClick={(event) => goToSection(event, item.link)}>
+                  <Link to={`#${item.link}`} className="mobileMenuItem" onClick={(event) => goToSection(event, item.link)}>
                     {language == "PL" ? item.name : item.link}
                   </Link>
                 </li>
-              ))
-            }
-            <LanguageSelector />
-          </ul>
-        </nav>
-        <div className="mobileIconWrapper">
-          <RiMenu5Line
-            size={24}
-            className="mobileMenuIcon"
-            title="Open menu"
-            onClick={() => openMenu()}
-          />
-        </div>
-      </>
-    );
-  }
-  
-  const MobileMenu = ({ mobileMenuDisplay, mobileMenuContentClass, closeMenu, goToSection, language }) => {
-    return (
-      <div className="mobileMenuOverlay" style={{ display: mobileMenuDisplay }}>
-        <div className={`mobileMenuWrapper ${mobileMenuContentClass}`}>
-          <div className="header">
-            <RiCloseCircleLine
-              size={34}
-              color="#FAFAFA"
-              title="Close menu"
-              onClick={() => closeMenu()}
-            />
-          </div>
-          <div className="content">
-            <ul>
-              {PageSections.map(item => {
-                return (
-                  <li key={item.name}>
-                    <Link to={`#${item.link}`} className="mobileMenuItem" onClick={(event) => goToSection(event, item.link)}>
-                      {language == "PL" ? item.name : item.link}
-                    </Link>
-                  </li>
-                )
-              })}
+              )
+            })}
+            <li>
               <LanguageSelector />
-            </ul>
-          </div>
+            </li>
+          </ul>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export {DesktopMenu, MobileMenu}
+export { DesktopMenu, MobileMenu }
